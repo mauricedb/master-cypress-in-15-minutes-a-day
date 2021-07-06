07fc6e01ce0b6f2708d7714bceffffa41726f7d6
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-07-06 09:45:16 +0200

    Settings

--- cypress/integration/block-buster-films/home.spec.js
+++ cypress/integration/block-buster-films/home.spec.js
@@ -1,5 +1,5 @@
 context('Block Buster Film Reviews', () => {
-  it('Has the correct title - Home Page', () =>{
+  it('Has the correct title - Home Page', () => {
     cy.visit('https://block-buster-film-reviews.azureedge.net')
     
     cy.title().should('equal', 'Block Buster Film Reviews')
