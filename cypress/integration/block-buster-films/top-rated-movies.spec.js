85eb1db88fc99a101b0af3b9bb583ba4be5611a9
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-07-06 14:30:19 +0200

    Top rated movies

---
+++ cypress/integration/block-buster-films/top-rated-movies.spec.js
@@ -0,0 +1,30 @@
+// top-rated-movies.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+context('Top rated movies', () => {
+  it('Has 24 movies per page', () => {
+    cy.visit('https://block-buster-film-reviews.azureedge.net/top-rated-movies')
+
+    cy.get('.movie-item-style-1').should('have.length', 24)
+  })
+
+  it('Has the correct title', () => {
+    cy.visit('https://block-buster-film-reviews.azureedge.net/top-rated-movies')
+
+    cy.title().should('equal', 'Top rated movies')
+    cy.get('h1')
+      .should('be.visible')
+      .and('have.text', 'Top rated movies')
+  })
+
+  it('The movie should be Dilwale Dulhania Le Jayenge', () => {
+    cy.visit('https://block-buster-film-reviews.azureedge.net/top-rated-movies')
+
+    cy.get('#movie-19404 > .mv-item-infor > h6 > a')
+      .should('have.text', 'Dilwale Dulhania Le Jayenge')
+  })
+})
