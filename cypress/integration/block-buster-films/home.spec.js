33d90722ffe2311e6b769edc1f4707e463f7cd5e
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-07-09 20:27:11 +0200

    Selecting DOM elements

--- cypress/integration/block-buster-films/home.spec.js
+++ cypress/integration/block-buster-films/home.spec.js
@@ -9,4 +9,38 @@ context('Block Buster Film Reviews', () => {
     cy.title().should('equal', 'Block Buster Film Reviews')
     cy.get('h1').should('have.text', 'Block Buster Film Reviews')
   })
+
+  context('The left navigation menu', () => {
+    it('The first navigation link points to Top rated movies', () => {
+      cy.get('.menu-left a:visible').first().should('have.text', 'Top rated movies')
+    })
+
+    it('The last  navigation link points to Celebrities', () => {
+      cy.get('.menu-left').find('a:visible').last().should('have.text', 'Celebrities')
+    })
+
+    it('Nested searches', () => {
+      cy.root()
+      cy.get('.menu-left').within(() => {
+        cy.root()
+        cy.get('a:visible').first().should('have.text', 'Top rated movies')
+        cy.get('a:visible').last().should('have.text', 'Celebrities')
+
+      })
+    })
+
+    it('The navigation links have the correct text', () => {
+      cy.get('.menu-left a:visible').eq(0).should('have.text', 'Top rated movies')
+      cy.get('.menu-left a:visible').eq(1).should('have.text', 'Popular movies')
+      cy.get('.menu-left a:visible').eq(-1).should('have.text', 'Celebrities')
+    })
+  })
+
+  context.skip('Anti-patterns', () => {
+    it("Don't use variables", () => {
+      // This is an anti-pattern. Don't use!
+      const menuLeft = cy.get('.menu-left')
+      menuLeft.find('a:visible').first().should('have.text', 'Top rated movies')
+    })
+  })
 })
